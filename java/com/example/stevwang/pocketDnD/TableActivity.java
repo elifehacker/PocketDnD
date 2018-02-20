7001619604b9dbda109a6a16ec3c2f3592ed7073
Stevwang <stevwang@au1.ibm.com>
2018-02-20 15:44:59 +1100

    the dialog activity could be called without an navigation bar, title bar. It only shows the dummy text in full screen mode.

--- java/com/example/stevwang/pocketDnD/TableActivity.java
+++ java/com/example/stevwang/pocketDnD/TableActivity.java
@@ -2,12 +2,29 @@ package com.example.stevwang.pocketDnD;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class TableActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_table);
+
+        View mContentView = findViewById(R.id.fullscreen_content);
+        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
+
     }
 }
