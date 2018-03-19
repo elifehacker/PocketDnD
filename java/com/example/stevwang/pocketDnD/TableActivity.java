006b49d39667f6c360d1f92c66a60838b9351641
Stevwang <stevwang@au1.ibm.com>
2018-03-19 17:59:47 +1100

    Can select equipment and multiple items, checking amount was automatic because it turned out inventory activity is recreated every time it is called from backpack/table activity

--- java/com/example/stevwang/pocketDnD/TableActivity.java
+++ java/com/example/stevwang/pocketDnD/TableActivity.java
@@ -1,6 +1,8 @@
 package com.example.stevwang.pocketDnD;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -26,7 +28,7 @@ public class TableActivity extends AppCompatActivity implements Serializable {
     static Equipment armor = null;
     static Equipment boots = null;
     static ArrayList<Consumable> items = null;
-    static int item_counter = 1;
+    static int item_counter = 0;
     static Context myContext;
 
     @Override
@@ -49,7 +51,6 @@ public class TableActivity extends AppCompatActivity implements Serializable {
         ImageButton bp_weapon = (ImageButton) findViewById(R.id.bp_im_weapon);
         bp_weapon.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
-
                 // Start NewActivity.class
                 Intent myIntent = new Intent(TableActivity.this,
                         InventoryActivity.class);
@@ -57,6 +58,44 @@ public class TableActivity extends AppCompatActivity implements Serializable {
                 startActivityForResult(myIntent, 101);//get item
             }
         });
+
+        ImageButton bp_armor = (ImageButton) findViewById(R.id.bp_im_armor);
+        bp_armor.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                // Start NewActivity.class
+                Intent myIntent = new Intent(TableActivity.this,
+                        InventoryActivity.class);
+                myIntent.putExtra("type", "armor");
+                startActivityForResult(myIntent, 101);//get item
+            }
+        });
+
+        ImageButton bp_boots = (ImageButton) findViewById(R.id.bp_im_boots);
+        bp_boots.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                // Start NewActivity.class
+                Intent myIntent = new Intent(TableActivity.this,
+                        InventoryActivity.class);
+                myIntent.putExtra("type", "boots");
+                startActivityForResult(myIntent, 101);//get item
+            }
+        });
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(TableActivity.this,
+                        InventoryActivity.class);
+                myIntent.putExtra("type", "item");
+                startActivityForResult(myIntent, 101);
+            }
+        };
+        ImageButton bp_item0 = (ImageButton) findViewById(R.id.bp_im_item0);
+        ImageButton bp_item1 = (ImageButton) findViewById(R.id.bp_im_item1);
+        ImageButton bp_item2 = (ImageButton) findViewById(R.id.bp_im_item2);
+        bp_item0.setOnClickListener(listener);
+        bp_item1.setOnClickListener(listener);
+        bp_item2.setOnClickListener(listener);
 
         Button back = (Button) findViewById(R.id.bp_back);
         back.setOnClickListener(new View.OnClickListener() {
@@ -72,12 +111,19 @@ public class TableActivity extends AppCompatActivity implements Serializable {
     @Override
     public void onActivityResult(int requestCode,int resultCode, Intent data)
     {
+        if(data == null) return;
+
         String item_id = data.getStringExtra("selected_id");
         String tagname = data.getStringExtra("selected_type");
+
+        if(item_id == null || tagname == null) return;
+
         String tag = "e";
         if (tagname.equals("item")){
             tag = "c";
             tagname += item_counter;
+            Log.d("debug", "tagname is "+tagname);
+
         }
         Log.d("debug", "looking for "+ tag+item_id);
 
@@ -86,22 +132,31 @@ public class TableActivity extends AppCompatActivity implements Serializable {
         ImageButton ib = (ImageButton)findViewById(image_id);;
         ib.setImageDrawable(getDrawable(drawable_id));
 
+        int item_int_id = Integer.parseInt(item_id);
+
         if(tagname.equals("weapon")){
-            if(this.weapon!=null) Home.getHome().getEquipments().add(this.weapon);
+            if(this.weapon!=null && item_int_id != this.weapon.getId()) Home.getHome().getEquipments().add(this.weapon);
             this.weapon = getEquipmentFromHome(item_id);
 
         }else if (tagname.equals("armor")){
-            if(this.armor!=null) Home.getHome().getEquipments().add(this.armor);
+            if(this.armor!=null && item_int_id != this.armor.getId()) Home.getHome().getEquipments().add(this.armor);
             this.armor = getEquipmentFromHome(item_id);
 
         }else if (tagname.equals("boots")){
-            if(this.boots!=null) Home.getHome().getEquipments().add(this.boots);
+            if(this.boots!=null && item_int_id != this.boots.getId()) Home.getHome().getEquipments().add(this.boots);
             this.boots = getEquipmentFromHome(item_id);
 
-        }else if (tagname.equals("item")){
+        }else if (tagname.contains("item")){
             if(this.items == null)
                 this.items = new ArrayList<Consumable>();
-            this.items.add(getItemsFromHome(item_id));
+            Consumable newConsumable = getItemsFromHome(item_id);
+            if(newConsumable != null) {
+                this.items.add(newConsumable);
+            }else{
+                showPopup("Not enough quantity. Maybe the backpack already had it.");
+                return;
+            }
+
             if(this.items.size() == 4) {
                 //remove and add the item back
                 Consumable con = this.items.remove(0);
@@ -109,6 +164,8 @@ public class TableActivity extends AppCompatActivity implements Serializable {
             }
             item_counter++;
             item_counter %= 3;
+            Log.d("debug", "item_counter is "+item_counter);
+
         }
 
 
@@ -130,12 +187,45 @@ public class TableActivity extends AppCompatActivity implements Serializable {
         int int_id = Integer.parseInt(id);
         for(Consumable eqp : Home.getHome().getConsumables()){
             if(eqp.getId() == int_id){
+                Log.d("debug", "found item at home ");
                 Consumable ret = eqp;
-                if(Home.getHome().getEquipments().remove(eqp))
+                if(Home.getHome().getConsumables().remove(eqp))
                     return ret;
             }
         }
         return null;
+    }
+
+    private void showPopup(String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(myContext);
+
+        builder.setTitle("Ops");
+        builder.setMessage(message);
+
+        builder.setPositiveButton("OKAY", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int which) {
+
+                // Do nothing, but close the dialog
+                dialog.dismiss();
+            }
+        });
+
+        /*
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                // Do nothing
+                dialog.dismiss();
+            }
+        });
+
+        */
+        AlertDialog alert = builder.create();
+        alert.show();
+
     }
 
 }
